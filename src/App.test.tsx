6f9bc487dc648a29f3485de62013f0a257b1192e
Vignika <20eg112132@anurag.edu.in>
2024-08-20 12:06:28 +0530

    adding covergae

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   screen.debug();
+
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+describe('App', () => {
+  it('renders hello react', () => {
+    render(<App />);
+
+    expect(screen.getByText('Hello React')).toBeInTheDocument();
+  });
 });
