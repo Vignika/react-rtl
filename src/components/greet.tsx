b71c13393bd2b8b2490b553d8e64102414770f30
Vignika <20eg112132@anurag.edu.in>
2024-08-20 15:23:22 +0530

    findbyrole

--- src/components/greet.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Greet = (props: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined }) => {
-  return (
-    <div>Hello {props.name}</div>
-  )
-}
-
